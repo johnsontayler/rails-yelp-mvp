f0bd302424797256c023325676be8cb4a2a6d37b
Tayler Johnson <johnsontayler@me.com>
2019-09-06 13:14:17 -0300

    middle of views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,6 +4,18 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @restaurants = Restaurant.all
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
   end
 end
